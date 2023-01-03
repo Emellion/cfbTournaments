4f8a87c5a2ebb82e7bd629435a26f179a430b8be
Emilio <irmscheremilio@gmail.com>
2023-01-04 00:16:59 +0100

    fixed rank icons this time hopefully

--- resources/views/tournaments.blade.php
+++ resources/views/tournaments.blade.php
@@ -5,7 +5,7 @@
                 <h1 class="font-semibold text-3xl text-white my-auto  whitespace-nowrap ml-10 tite-hide">TOURNAMENTS. &#160;</h1>
                 <h1 class="font-light text-xl text-white my-auto whitespace-nowrap tite-hide ">COMPETE AGAINST OTHER TEAMS.</h1>
             </div>
-            <img src="/neon.png" class="">
+            <img src="neon.png" class="">
         </div>
         <div class="max-w-9xl mx-auto sm:px-6 lg:px-8">
             <form action="/tournaments">
@@ -17,8 +17,7 @@
                     <button class="flex py-6 sm:px-20 flex-row sm:flex-col justify-evenly"
                         style="background-color: #04040a; width:50%">
                         <div class="flex font-medium text-white px-4 text-lg">
-                            <div class="flex rounded-full px-5 py-3"><img src="/Diamond_3_rank.png" width="50rem"
-                                    class=""></div>
+                            <div class="flex rounded-full px-5 py-3"><img src="Diamond_3_rank.png" width="50rem" alt="" class=""></div>
                         </div>
                         <div class="flex font-light text-white px-4 text-lg my-auto tite-hide">
                             <p>Diamond - Ascendant</p>
@@ -42,7 +41,7 @@
                     <button class="flex py-6 sm:px-20 flex-row sm:flex-col justify-evenly"
                         style="background-color: #04040a; width:50%">
                         <div class="flex font-medium text-white px-4 text-lg">
-                            <div class="flex rounded-full px-5 py-3"><img src="/Diamond_3_rank.png" width="50rem"
+                            <div class="flex rounded-full px-5 py-3"><img src="Diamond_3_rank.png" width="50rem"
                                     class=""></div>
                         </div>
                         <div class="flex font-light text-white px-4 text-lg my-auto tite-hide">
